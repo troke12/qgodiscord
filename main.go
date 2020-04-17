5aee4cd66d8fe846ec574a3863431336105bf266
Quan Truong <quantt86@gmail.com>
2020-04-17 09:56:57 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	//"crypto/tls"
+	"flag"
+	"fmt"
+	//"io/ioutil"
+	"net/http"
+	//"os"
+	//"strings"
+	"encoding/json"	
+	"bytes"
+)
+
+type Message struct {
+	Username string `json:"username"`
+    Content string `json:"content"`
+}
+
+func main() {
+
+	//Discord webhook api url flag
+	var api string
+	flag.StringVar(&api, "a", "",  "Discord webhook URL of server to send message to")
+	
+	var message string
+	flag.StringVar(&message, "m", "", "Message to send")
+	
+	var user string
+	flag.StringVar(&user, "u", "Captain Hook", "Discord Username")
+	
+	//debug flags
+	//var debug bool
+	//flag.BoolVar(&debug, "debug", false, "Display debug logs with -debug option")
+	
+	flag.Parse()
+	fmt.Println("username", user)
+	fmt.Println("message", message)
+	
+	myMsg := Message{Username:user,Content:message}
+	b, err := json.Marshal(myMsg)
+	
+	if err != nil {
+		fmt.Println("error:", err)	
+	}
+	//os.Stdout.Write(b)
+	SendMessage(api, string(b))
+}
+
+
+func SendMessage(api string, jsonData string) {
+
+	var jsonStr = []byte(jsonData)
+
+	req, err := http.NewRequest("POST", api, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+}
